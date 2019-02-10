3fd6691889f0aecda423d8d8e2703653776d0a08
Alex Morega <alex@grep.ro>
2019-02-11 01:29:54 +0200

    Handle errors responsibly

--- vmck/nomad.py
+++ vmck/nomad.py
@@ -5,5 +5,12 @@ from urllib.parse import urljoin
 api = urljoin(settings.NOMAD_URL, 'v1')
 
 
+def response(res):
+    if 200 <= res.status_code < 300:
+        return res.json()
+    else:
+        raise RuntimeError(f"Request failed: {res.text}")
+
+
 def jobs():
-    return requests.get(f'{api}/jobs').json()
+    return response(requests.get(f'{api}/jobs'))
