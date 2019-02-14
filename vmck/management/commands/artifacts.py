a39d85eaf5fb6da06c2aebc67d2d476005f7f764
Alex Morega <alex@grep.ro>
2019-02-14 12:27:17 +0200

    show artifacts

---
+++ vmck/management/commands/artifacts.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from ...models import Job
+from ... import jobs
+
+
+class Command(BaseCommand):
+    help = "Kills a job."
+
+    def add_arguments(self, parser):
+        parser.add_argument('job_id')
+
+    def handle(self, job_id, *args, **options):
+        job = Job.objects.get(id=job_id)
+        for artifact in job.artifact_set.all():
+            print(f"===== {artifact.name} =====")
+            print(artifact.data.decode('utf8', errors='replace'))
