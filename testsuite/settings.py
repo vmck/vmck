b9f21327f818fd5b82ff27f85aa692c2bc465398
Alex Morega <alex@grep.ro>
2019-03-24 12:11:43 +0200

    `DEBUG` flag for tests

--- testsuite/settings.py
+++ testsuite/settings.py
@@ -4,12 +4,20 @@ import secrets
 from datetime import datetime
 from vmck.base_settings import *
 
+def is_true(value):
+    text = (value or '').lower().strip()
+    return text in ['1', 'yes', 'true', 'on', 'enabled']
+
+
 vocabulary_64 = string.ascii_letters + string.digits + '.+'
 
 def random_code(length, vocabulary=vocabulary_64):
     return ''.join(secrets.choice(vocabulary) for _ in range(length))
 
+
 SECRET_KEY = random_code(43)
+
+DEBUG = is_true(os.environ.get('DEBUG'))
 
 DATABASES = {
     'default': {
@@ -24,4 +32,3 @@ NOMAD_DEPLOYMENT_NAME = f"test {datetime.now().strftime('%H:%M:%S')}"
 github_image = 'https://github.com/mgax/vmck-images/raw/master/artful.qcow2'
 QEMU_IMAGE_URL = os.environ.get('TESTING_QEMU_IMAGE_URL', github_image)
 QEMU_IMAGE_USERNAME = 'ubuntu'
-QEMU_IMAGE_PASSWORD = 'ubuntu'
