341e45ef2206d494a974e6ca9a603f5aec3e74fd
dixi3 <23087210+dixi3@users.noreply.github.com>
2019-05-25 09:42:22 +0300

    Added sentry logging

--- vmck/settings.py
+++ vmck/settings.py
@@ -1,6 +1,8 @@
 import os
+import sentry_sdk
 from .utils import is_true
 from .base_settings import *
+from sentry_sdk.integrations.django import DjangoIntegration
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -25,3 +27,8 @@ QEMU_IMAGE_URL = os.environ.get('QEMU_IMAGE_URL')
 
 _ssh_username = SSH_USERNAME
 SSH_USERNAME = os.environ.get('SSH_USERNAME', _ssh_username)
+
+sentry_sdk.init(
+    dsn="https://cfc9c776b9834e078b54b96f6826a0f6@sentry.io/1467831",
+    integrations=[DjangoIntegration()]
+)
