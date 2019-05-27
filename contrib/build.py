6fc54619c7353b86db70866e5a7eb4216d8574cb
Alex Morega <alex@grep.ro>
2019-05-27 21:37:51 +0200

    Build images for Vagrant happiness

--- contrib/build.py
+++ contrib/build.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+# apt install cloud-image-utils qemu-kvm
+
 import logging
 from pathlib import Path
 from argparse import ArgumentParser
@@ -18,11 +20,7 @@ BIONIC_URL = (
     'bionic-server-cloudimg-amd64.img'
 )
 
-SSH_PUBKEY = (
-    'ssh-ed25519 '
-    'AAAAC3NzaC1lZDI1NTE5AAAAIM9QBDGxYk2MjJHMa30Ut166/s61Yk1ieTn3aXjAnnvP '
-    'vmck'
-)
+SSH_PUBKEY = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key'
 
 CLOUD_INIT_YML = f"""\
 #cloud-config
