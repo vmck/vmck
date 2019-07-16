4dd696245663ec85744eb4254b75e425f1e285b2
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-07-16 20:18:19 +0300

    CI test for nomad.vmck
    Issue #27 [WIP]

---
+++ ci/test-alive-on-cluster.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+set +e
+echo "Starting cluster.py"
+python3 /opt/node/examples/cluster.py
+echo "cluster.py run successfully"
+
+i=0
+
+while [ $i -le 180 ]; do
+  if curl --output /dev/null --silent --head --fail "10.66.60.1:9995"; then
+    echo "Vmck is up and running"
+    exit 0
+  else
+    sleep 5
+    i=$((i + 5))
+  fi
+done
+
+exit -1
