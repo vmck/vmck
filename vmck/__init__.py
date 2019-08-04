1ecff2435432776f18b1568dc9d43911f12ba12c
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-04 14:47:08 +0300

    Jobs get killed on SIGTERM

---
+++ vmck/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'vmck.apps.VmckConfig'
--- vmck/apps.py
+++ vmck/apps.py
@@ -1,5 +1,18 @@
 from django.apps import AppConfig
 
+import signal
+
 
 class VmckConfig(AppConfig):
     name = 'vmck'
+
+    @staticmethod
+    def signalHandler(signalNumber, frame):
+        from .models import Job
+        from .jobs import kill
+
+        for job in Job.objects.all():
+            kill(job)
+
+    def ready(self):
+        signal.signal(signal.SIGTERM, VmckConfig.signalHandler)
