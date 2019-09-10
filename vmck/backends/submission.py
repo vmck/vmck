bb19f18b6972d07f5077a1e47673887cc97cd5e1
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-10 17:26:42 +0300

    Template submission.sh

--- vmck/backends/submission.py
+++ vmck/backends/submission.py
@@ -4,6 +4,12 @@ def task(job, options):
         'driver': 'docker',
         'config': {
             'image': 'vmck/vagrant-vmck:submission',
+            'volumes': [
+                "local/submission.sh:/src/submission.sh",
+                "local/Vagrantfile:/src/Vagrantfile",
+            ],
+            'command': '/bin/bash',
+            'args': ['/src/submission.sh'],
             'force_pull': True,
         },
         'env': {
@@ -13,6 +19,37 @@ def task(job, options):
             'INTERFACE_ADDRESS': options['env']['interface_address'],
             'VMCK_JOB_ID': str(job.id),
         },
+        'templates': [
+            {
+                "DestPath": "local/submission.sh",
+                "EmbeddedTmpl": '''#!/bin/bash -ex
+                                   trap "vagrant destroy -f" EXIT
+                                   curl -X GET ${DOWNLOAD_ARCHIVE_URL} > submission.zip
+                                   curl -X GET ${DOWNLOAD_SCRIPT_URL} > checker.sh
+                                   vagrant up
+                                   vagrant ssh -- < checker.sh > result.out
+                                   data="$(base64 result.out)"
+                                   curl -X POST "http://${INTERFACE_ADDRESS}/done/" -d "{\"token\": \"${DOWNLOAD_ARCHIVE_URL}\", \"output\": \"${data}\"}"''',  # noqa: E501
+            },
+            {
+                "DestPath": "local/Vagrantfile",
+                "EmbeddedTmpl": '''Vagrant.configure("2") do |config|
+        config.vm.box = 'base'
+
+        if Vagrant.has_plugin?('vagrant-env')
+            config.env.enable
+        end
+
+        config.nfs.functional = false
+        config.vm.provision 'shell', inline: 'mv /vagrant/submission.zip .; \
+                                                unzip submission.zip; \
+                                                chown -R vagrant:vagrant .'
+        config.vm.provider :vmck do |vmck|
+            vmck.vmck_url = ENV['VMCK_URL']
+        end
+    end'''
+            },
+        ],
         'resources': {
             'MemoryMB': options['env']['memory'],
             'CPU': options['env']['cpu_mhz'],
