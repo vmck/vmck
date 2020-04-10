42388487258ee955c096429bb421bc5b7e892364
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-04-10 18:30:58 +0300

    Bad import

--- vmck/management/commands/waitress.py
+++ vmck/management/commands/waitress.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from vmck.wsgi import app
+from vmck.wsgi import application
 from vmck.settings import APP_THREAD_COUNT
 
 from waitress import serve
@@ -9,4 +9,4 @@ class Command(BaseCommand):
     help = 'Start the app on port 8100'
 
     def handle(self, *args, **options):
-        serve(app, port='8000', threads=APP_THREAD_COUNT)
+        serve(application, port='8000', threads=APP_THREAD_COUNT)
