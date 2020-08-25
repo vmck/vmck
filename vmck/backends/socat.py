d5b3ecf16a49acdf57730ce208632a43220faabd
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-08-25 19:22:52 +0300

    Revert "go back to tcp checks"
    This reverts commit d3fc9888fbdc3069e8105a50d7625add31b31ea7.

--- vmck/backends/socat.py
+++ vmck/backends/socat.py
@@ -1,5 +1,12 @@
 second = 1000000000
 
+check_script = """\
+(
+    set -x
+    echo | nc $NOMAD_IP_ssh $NOMAD_PORT_ssh | grep -q 'SSH-'
+) 2>&1
+"""
+
 
 def services(job):
     name = f"vmck-{job.id}-ssh"
@@ -12,7 +19,9 @@ def services(job):
                 {
                     "Name": f"{name} ssh",
                     "InitialStatus": "critical",
-                    "Type": "tcp",
+                    "Type": "script",
+                    "Command": "/bin/sh",
+                    "Args": ["-c", check_script],
                     "PortLabel": "ssh",
                     "Interval": 1 * second,
                     "Timeout": 1 * second,
